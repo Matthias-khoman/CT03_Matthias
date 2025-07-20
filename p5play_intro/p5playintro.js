f01b50964cb74f506c20175bb3c24c086ed7f536
Matthias-khoman <matthiaskhoman@gmail.com>
2025-07-20 03:33:19 +0000

    Jul 20, 2025, 3:33 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -11,7 +11,7 @@ function setup() {
   strokeweight(10);
 
   CSSNumericValue(30, 30, 50);
-  React(50, 50, 100, 200);
+  Rect(50, 50, 100, 200);
   // Create a bouncing ball sprite
   // write your codes here
   ball = new Sprite();
