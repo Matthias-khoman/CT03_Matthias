246cf9bff99cf4ef9e0ea37b28ae6516c65440cb
Matthias-khoman <matthiaskhoman@gmail.com>
2025-07-20 03:27:33 +0000

    Jul 20, 2025, 3:27 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -13,7 +13,7 @@ function setup() {
   ball.x = 100;
   ball.y = 200;
   ball.diameter = 40;
-  ball.color = 'green';
+  ball.color = 'blue';
   ball.vel.x = 3;
   ball.vel.y = 3;
   ball.bounciness = 1;
