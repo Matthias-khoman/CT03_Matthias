a386772254a7c033957dadb8c2a762c7901a2a33
Matthias-khoman <matthiaskhoman@gmail.com>
2025-07-20 03:31:29 +0000

    Jul 20, 2025, 3:31 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -6,7 +6,8 @@ function setup() {
   // Basic shape testing
   // write your codes here
   // End Basic shape testing
-  Fillin('skyblue')
+  Fill('skyblue')
+  stroke
   // Create a bouncing ball sprite
   // write your codes here
   ball = new Sprite();
