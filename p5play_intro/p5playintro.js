7e2bdeb6b60e6f1e2bbeb6de6717650a4e5e6833
Matthias-khoman <matthiaskhoman@gmail.com>
2025-07-20 03:51:48 +0000

    Jul 20, 2025, 3:51 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -38,7 +38,7 @@ function draw() {
   // write your codes here
   background(250)
 
-  if (ball.x  < 0+ ball.diameter / 2 || ball.x + ball.diameter > width){
+  if (ball.x < 0+ ball.diameter / 2 || ball.x + ball.diameter > width){
     ball.vel.x  *= -1;
   }
 
