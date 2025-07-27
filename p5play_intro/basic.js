a36ce63921ec9f6bfd7627faa457b78080a4f8b2
Matthias-khoman <matthiaskhoman@gmail.com>
2025-07-27 03:45:49 +0000

    Jul 27, 2025, 3:45 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -41,9 +41,9 @@ function setup() {
   for(let i = 20; i>0; i--){
     console.log(i);
   }
-  for(let i = 3; i <= 46; i + 3){
-    console.log(i)
-  }
+  // for(let i = 3; i <= 46; i + 3){
+  //   console.log(i)
+  // }
 }
 
 function draw(){
