bad3d59324163d6642ab5febb5b61bd9b1133890
Matthias-khoman <matthiaskhoman@gmail.com>
2025-07-27 04:18:17 +0000

    Jul 27, 2025, 4:18 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -53,10 +53,10 @@ function setup() {
   let age = 13;
   if(age <= 9){
     console.log("Lower primary " + age);
-    text(age, 100, 100)
+    text("Lower primary: " + age, 100, 100)
   }else if(age<=12){
     console.log("Upper primary " + age);
-    text("Upper primary: " +age, 150, 150)
+    text("Upper primary: " + age, 150, 150)
   }else{
     console.log("Secondary school " + age);
     text("secondary school: " + age, 200, 200)
