b71820bd9666b219f1d39de2e98a3f2785604bc9
Matthias-khoman <matthiaskhoman@gmail.com>
2025-07-27 03:57:51 +0000

    Jul 27, 2025, 3:57 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -34,8 +34,8 @@ function setup() {
   
   let product = a * b;
   // console.log("Sum of a and b: ", sum);
-  console.log("Product of a and b: ", product);
-  console.log(area);
+  // console.log("Product of a and b: ", product);
+  // console.log(area);
   // for(let i = 10; i<21; i++){
   //   console.log(i);
   // }
@@ -45,7 +45,7 @@ function setup() {
   // for(let i = 3; i <= 46; i += 3){
   //   console.log(i);
   // }
-  // total = 0
+  total = 0;
   for(let i = 2; i <= 20; i += 2){
     console.log(i);
     total += i;
