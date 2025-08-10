7ee49ecce72d61b78e707b6380c178745217ce5e
Matthias-khoman <matthiaskhoman@gmail.com>
2025-08-10 03:51:21 +0000

    Aug 10, 2025, 3:51 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -36,14 +36,14 @@
   function draw(){
     image(bg, 0, 0, width, height);
 
-    if(kb.presses('space')){
-      bird.vel.y = -15;
-      bird.sleeping = false;
-    }
+    // if(kb.presses('space')){
+    //   bird.vel.y = -15;
+    //   bird.sleeping = false;
+    // }
 
-    if(mouse.presses()){
-      new Sprite(mouse.x, 200, 30, 30, 'dynamic')
-    }
+    // if(mouse.presses()){
+    //   new Sprite(mouse.x, 200, 30, 30, 'dynamic')
+    // }
 
     fill("blue");
     textSize(14);
