ba43c15bb50c7c427d38c1c2db8f1d1c1c228690
Matthias-khoman <matthiaskhoman@gmail.com>
2025-08-10 03:45:29 +0000

    Aug 10, 2025, 3:45 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -5,7 +5,8 @@
     flapMidImg = loadImage('assets/yellowbird-midflap.png')
     bg = loadImage('assets/background-day.png')
     base = loadImage('assets/base.png')
-    flapUpImg = loadImage('flappybird/assets/yellowbird-upflap.png')
+    flapUpImg = loadImage('assets/yellowbird-upflap.png')
+    flap
   }
 
   function setup(){
