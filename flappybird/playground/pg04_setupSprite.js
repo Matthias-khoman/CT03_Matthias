2343372752995c44fe24f122cededca10068e39d
Matthias-khoman <matthiaskhoman@gmail.com>
2025-08-10 03:32:41 +0000

    Aug 10, 2025, 3:32 AM

--- flappybird/playground/pg04_setupSprite.js
+++ flappybird/playground/pg04_setupSprite.js
@@ -45,7 +45,7 @@
 
     FileList("blue");
     textSize(14);
-    text("vel.y: " + bird.vel.ytoFixed(2), 10, 20);
+    text("vel.y: " + bird.vel.y.toFixed(2), 10, 20);
     text("isMoving: " + bird.isMoving, 10, 40);
     text("sleeping: " + bird.sleeping, 10, 60);
 
