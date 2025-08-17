3cebd3f03ba5a0714fd65d993bb4852decf4aa2b
Matthias-khoman <matthiaskhoman@gmail.com>
2025-08-17 03:13:35 +0000

    Aug 17, 2025, 3:13 AM

--- flappybird/playground/pg05a_spriteanimation.js
+++ flappybird/playground/pg05a_spriteanimation.js
@@ -8,7 +8,7 @@
     base = loadImage('assets/base.png')
     flapUpImg = loadImage('assets/yellowbird-upflap.png')
     flapDownImg = loadImage('assets/yellowbird-downflap.png')
-    pipeImg = loadImage('assets/pipe-')
+    pipeImg = loadImage('assets/pipe-green.png')
 
   }
 
