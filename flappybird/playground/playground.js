b0068bbe83003130df384f019b404799a8fbdb50
Matthias-khoman <matthiaskhoman@gmail.com>
2025-08-17 03:41:15 +0000

    Aug 17, 2025, 3:41 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -84,7 +84,7 @@
   function spawnPipePair() {
     let gap = 50;
     let midY = height/2
-    bottomPipe = new Sprite(100, midY + gap + 200, 52, 320, 'static');
+    bottomPipe = new Sprite(400, midY + gap + 200, 52, 320, 'static');
     bottomPipe.img = pipeImg;
     
     pipeGroup.add(bottomPipe);
