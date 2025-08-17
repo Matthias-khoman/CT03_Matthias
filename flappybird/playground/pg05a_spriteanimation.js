2b61fc30f7e92691e93b68f9779cd76322ad397c
Matthias-khoman <matthiaskhoman@gmail.com>
2025-08-17 03:25:11 +0000

    Aug 17, 2025, 3:25 AM

--- flappybird/playground/pg05a_spriteanimation.js
+++ flappybird/playground/pg05a_spriteanimation.js
@@ -3,7 +3,7 @@
   let flapMidImg, bg, base;
   let pipeImg;
   let pipeGroup;
-  let botot
+  let bottomPipe, topPipe
   function preload(){
     flapMidImg = loadImage('assets/yellowbird-midflap.png');
     bg = loadImage('assets/background-day.png');
