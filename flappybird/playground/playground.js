c983b45b9bc9784eb7f051c3c4f46b3d5f510b42
Matthias-khoman <matthiaskhoman@gmail.com>
2025-08-24 04:29:38 +0000

    Aug 24, 2025, 4:29 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -179,7 +179,7 @@ function drawScore(x, y, score, digitWidth, digitHeight){
   scoreDigits.removeAll();
   let scoreStr = str(score)
   let totalWidth = scoreStr.length * digitWidth
-  let start
+  let startX = x -
 
 }
 
