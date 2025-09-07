4a2bfb5e7f2594d58c12d0762933851403718f9e
Matthias-khoman <matthiaskhoman@gmail.com>
2025-09-07 03:27:39 +0000

    Sep 7, 2025, 3:27 AM

--- p5play_intro/testanswers.js
+++ p5play_intro/testanswers.js
@@ -5,8 +5,8 @@ let ball;
 
 function setup(){
     new Canvas(600, 400);
-    let abc = new Sprite();
-    abc.diameter = 50;
+    //let abc = new Sprite();
+    //abc.diameter = 50;
     abc.y = 50;
     world.gravity.y = 1;
 }
