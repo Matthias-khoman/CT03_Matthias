03d70d67f009dc0d49d70826577ba70b0524d9ce
Matthias-khoman <matthiaskhoman@gmail.com>
2025-09-07 04:09:59 +0000

    Sep 7, 2025, 4:09 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -150,7 +150,8 @@ function draw() {
     text('bird.x: ' + bird.x.toFixed(2), 10, 80);
   }
 
-  drawScore(width/2,20, score, 24, 36);4
+  drawScore(width/2,20, score, 24, 36);
+  
 
 } // draw()
 
