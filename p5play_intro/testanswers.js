486119ec6f8392428f9e47cd2c13485b246dd054
Matthias-khoman <matthiaskhoman@gmail.com>
2025-09-07 03:42:34 +0000

    Sep 7, 2025, 3:42 AM

--- p5play_intro/testanswers.js
+++ p5play_intro/testanswers.js
@@ -9,7 +9,8 @@ function setup(){
     //abc.diameter = 50;
     //abc.y = 50;
     world.gravity.y = 1;
-    let floor = new Sprite(100, 360, 600, 10, 'static');
+    let floor = new Sprite();
+    floor.w = 10
 }
 
 //forever block
