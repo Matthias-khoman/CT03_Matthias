a822b0765a8d0b7d8dbb0790a13b01782348116d
Matthias-khoman <matthiaskhoman@gmail.com>
2025-09-21 03:15:00 +0000

    Sep 21, 2025, 3:15 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -179,8 +179,8 @@ function spawnPipePair(){
   topPipe = new Sprite(bird.x + 400, midY - gap / 2 - 200, 52, 320, 'static');
   topPipe.img = pipe;
   topPipe.rotation = 180;
-  
-  topPipe.passed = false; // bird passx through
+
+  topPipe.passed = false; // bird pass through
 
   // create the bottom pipe sprite
   bottomPipe = new Sprite(bird.x + 400, midY + gap / 2 + 200, 52, 320, 'static');
