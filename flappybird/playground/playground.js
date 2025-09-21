17df28af251a2e035c498a976e2249b98ee8afd6
Matthias-khoman <matthiaskhoman@gmail.com>
2025-09-21 03:47:03 +0000

    Sep 21, 2025, 3:47 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -114,6 +114,7 @@ function draw() {
     if (kb.presses('space') || mouse.presses()) {
       bird.vel.y = -5; // which direction do you think this is?
       bird.sleeping = false; // wake up if sleeping
+      flap
     }
     
     // Activity: Change image according to flying action/ falling
