b6533b6b09b0b41d80edb2266980e532dbf53169
Matthias-khoman <matthiaskhoman@gmail.com>
2025-09-21 03:44:20 +0000

    Sep 21, 2025, 3:44 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -38,7 +38,7 @@ function preload() {
       numberImages[i] = loadImage('assets/' + i + '.png')
     }
 
-    flapSound = createAudio()
+    flapSound = createAudio('assets')
 }
 
 function setup() {
