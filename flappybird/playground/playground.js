5d3a230b874da7922b339e6cd3c6f5ebf8d75af9
Matthias-khoman <matthiaskhoman@gmail.com>
2025-09-21 03:19:51 +0000

    Sep 21, 2025, 3:19 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -139,7 +139,8 @@ function draw() {
     }
 
     for(let pipe of pipeGroup){
-      let pipeRightEdge = pipe.x + pipe.w/2
+      let pipeRightEdge = pipe.x + pipe.w/2;
+      
     }
 
     // End Game on Collision
