e7280863e35afbaa3dbc574a2b451cb1c216a689
Matthias-khoman <matthiaskhoman@gmail.com>
2025-10-05 04:20:31 +0000

    Oct 5, 2025, 4:20 AM

--- ml5/handpose_balloon.js
+++ ml5/handpose_balloon.js
@@ -24,7 +24,7 @@ function setup(){
     fingerTip = new Sprite()
     fingerTip.diameter = 60;
     fingerTip.collider = 'kinematic'
-    fingerTip.color = 'rgba(0, 255, 0, 0, 0.5)'
+    fingerTip.color = 'rgba(0, 255, 0, 0.05)'
 
     let constraints = {
         video:{
