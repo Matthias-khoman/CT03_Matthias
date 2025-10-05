dad990488b9f144900862f9444b394b55976699e
Matthias-khoman <matthiaskhoman@gmail.com>
2025-10-05 03:54:13 +0000

    Oct 5, 2025, 3:54 AM

--- ml5/handpose_basic.js
+++ ml5/handpose_basic.js
@@ -46,7 +46,7 @@ function draw(){
     for(let i = 0; i < hands.length; i++){
         let hand = hands[i]
 
-        for(let j = 0; j < hand.keypoints.length; i++){
+        for(let j = 0; j < hand.keypoints.length; j++){
             let keypoint = hand.keypoints[j]
 
             circle(keypoint.x, keypoint.y, 10);
