2b5cebbc823d171d22936876a4db9cfe347eb5d3
Matthias-khoman <matthiaskhoman@gmail.com>
2025-10-19 03:37:51 +0000

    Oct 19, 2025, 3:37 AM

--- ml5/handpose_balloon.js
+++ ml5/handpose_balloon.js
@@ -122,7 +122,7 @@ function keyPressed(){
         // reset balloon state
         balloon.x = width/2
         balloon.y = 100;
-        balloon.vel.x;
-        balloon.vel
+        balloon.vel.x= 0;
+        balloon.vel.y = 0
     }
 }
