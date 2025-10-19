76431b2991f96cb04ac490215d05d6f7eb4ef591
Matthias-khoman <matthiaskhoman@gmail.com>
2025-10-19 03:55:06 +0000

    Oct 19, 2025, 3:55 AM

--- ml5/handpose_balloon.js
+++ ml5/handpose_balloon.js
@@ -82,9 +82,7 @@ function setup(){
 function draw(){
     image(video,0,0, videoW, videoH)
 
-    if(balloon.collides(fingerTip)){
-        playsound(bounceSound)
-    }
+
 
     if(gameStarted === false){
         textSize(28);
