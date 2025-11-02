7d807884178288de61723926cf8eb9c4fb04da58
Matthias-khoman <matthiaskhoman@gmail.com>
2025-11-02 03:26:01 +0000

    Nov 2, 2025, 3:26 AM

--- p5play_intro/basic_input.js
+++ p5play_intro/basic_input.js
@@ -1,49 +1,49 @@
-let inputText;
-let userText = 'Your Text here';
-let inputAge;
-let ageText = 'Your age';
-let colorPicker;
+// let inputText;
+// let userText = 'Your Text here';
+// let inputAge;
+// let ageText = 'Your age';
+// let colorPicker;
 
 
-function setup(){
-    createCanvas(600, 400);
+// function setup(){
+//     createCanvas(600, 400);
 
-    inputText1 = createInput();
-    inputText1.position(width/2 - 80, height - 150);
-    inputText1.input(updateText);
+//     inputText1 = createInput();
+//     inputText1.position(width/2 - 80, height - 150);
+//     inputText1.input(updateText);
 
-    inputAge = createInput();
-    inputAge.position(width/2 - 80, height - 175);
-    inputAge.input(updateAge);
+//     inputAge = createInput();
+//     inputAge.position(width/2 - 80, height - 175);
+//     inputAge.input(updateAge);
 
-    colorPicker = createColorPicker("#459ca3ff");
-    colorPicker.position(200, 32);
+//     colorPicker = createColorPicker("#459ca3ff");
+//     colorPicker.position(200, 32);
 
 
-}
-function draw(){
-    background(colorPicker.value());
+// }
+// function draw(){
+//     background(colorPicker.value());
 
-    fill(255)
-    rect(50, 100, 500, 100, 25);
+//     fill(255)
+//     rect(50, 100, 500, 100, 25);
     
 
-    textSize(16);
-    textAlign(LEFT);
-    fill(0);
-    text("Pick a color", 20, 50)
-    text("Enter yiur name", width/2 - 180, height - 160 )
+//     textSize(16);
+//     textAlign(LEFT);
+//     fill(0);
+//     text("Pick a color", 20, 50)
+//     text("Enter yiur name", width/2 - 180, height - 160 )
 
-    fill(0);
-    textSize(28);
-    textAlign(CENTER,CENTER);
-    text(userText, width/2, 180);
+//     fill(0);
+//     textSize(28);
+//     textAlign(CENTER,CENTER);
+//     text(userText, width/2, 180);
 
-}
+// }
 
-function updateText(){
-    userText = this.value();
-}
-function updateAge(){
-    ageText = this.value();
-}
+// function updateText(){
+//     userText = this.value();
+// }
+// function updateAge(){
+//     ageText = this.value();
+// }
