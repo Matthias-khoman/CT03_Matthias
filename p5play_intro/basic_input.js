f4ec4c204a3ee067ea9a6d9880b790198f11d6d8
Matthias-khoman <matthiaskhoman@gmail.com>
2025-11-02 04:18:38 +0000

    Nov 2, 2025, 4:18 AM

--- p5play_intro/basic_input.js
+++ p5play_intro/basic_input.js
@@ -109,7 +109,7 @@ function setup(){
     textSize(20);
     textAlign(CENTER, CENTER);
     text("Enter a noun: ", width/2 - 150, height - 570)
-    text("Enter a noun: ", width/2 - 150, height - 570)
+    text("Enter a verb: ", width/2 - 150, height - 570)
 
     noun = createInput('e.g dog');
     noun.position(width/2, 20);
