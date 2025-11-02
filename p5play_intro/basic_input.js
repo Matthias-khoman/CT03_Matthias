207710048fe7a47c1dfabe0f36657f556972a83a
Matthias-khoman <matthiaskhoman@gmail.com>
2025-11-02 03:40:38 +0000

    Nov 2, 2025, 3:40 AM

--- p5play_intro/basic_input.js
+++ p5play_intro/basic_input.js
@@ -67,7 +67,7 @@ function draw(){
     fill(colorPicker.value());
     textSize(28);
     textAlign(CENTER, CENTER);
-    text(Text, 200, 170);
+    text(Text, width / 2, );
 }
 function updateText(){
     Text = this.value();
